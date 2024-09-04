411a222b13a45b58a3d5bbbd6109be6258ade389
Samuel <samuferfort@gmail.com>
2024-09-04 05:27:41 +0200

    server listen

--- app.js
+++ app.js
@@ -37,6 +37,6 @@ app.use((err, req, res, next) => {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(port, '0.0.0.0', () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
