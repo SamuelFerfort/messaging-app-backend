555265013ada965728bacd3194352fbef958a358
Samuel <samuferfort@gmail.com>
2024-09-08 01:10:33 +0200

    add unique on first name and last name

--- addAllUsersToGroupChat.js
+++ addAllUsersToGroupChat.js
@@ -3,43 +3,14 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 async function addAllUsersToOdinProjectChads() {
-
-
-
   try {
     // Create the new group chat
-    
 
-    const newGroupChat = await prisma.chat.findFirst({
-      where: {
-        name: "The Odin Project Chads",
-        isGroup: true,
-      },
-    });
+    const deletion = prisma.user.delete({ where: { lastName: "ross" } });
 
-    // Fetch all existing users
-    const allUsers = await prisma.user.findMany();
-
-    // Add all users to the new group chat
-    const updatePromises = allUsers.map((user) =>
-      prisma.chat.update({
-        where: { id: newGroupChat.id },
-        data: {
-          users: {
-            connect: { id: user.id },
-          },
-        },
-      })
-    );
-
-    await Promise.all(updatePromises);
-
-    console.log(
-      "All users have been added to 'The Odin Project Chads' group chat."
-    );
-    return newGroupChat;
+    console.log("bob ross deleted");
   } catch (error) {
-    console.error("Error adding users to the group chat:", error);
+    console.error("Error deleting ross", error);
     throw error;
   } finally {
     await prisma.$disconnect();
